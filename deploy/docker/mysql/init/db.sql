3147ac6c746949ac78e5aabacd04f1fed1317823
Brennon Loveless <brennon.loveless@gmail.com>
2019-08-12 21:22:44 -0400

    Updated k8s, build, and docker.

--- deploy/docker/mysql/init/db.sql
+++ deploy/docker/mysql/init/db.sql
@@ -35,7 +35,7 @@ VALUES ('First autumn morning',
 CREATE TABLE users (
   id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(191) NOT NULL,
   hashed_password CHAR(60) NOT NULL,
   created DATETIME NOT NULL
 );
